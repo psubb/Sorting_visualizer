81f24144f75154940d1e9dca57072e0ccbb87805
psubb <pranavsubbiah@gmail.com>
2024-06-25 17:40:30 -0700

    Debug changes

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import logging
 from app import create_app
 
+logging.basicConfig(level=logging.DEBUG)
 app = create_app()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    logging.debug("Starting Flask app...")
     app.run(host="0.0.0.0", port=5000)
